dbe31d319a5ee404bcb4920bc5d701d38e589602
SmearingYash <yashsnab0@gmail.com>
2023-06-27 15:02:38 +0545

    example search bar change

--- frontend/src/component/SearchInputEl.js
+++ frontend/src/component/SearchInputEl.js
@@ -46,7 +46,7 @@ const SearchInputEl = () => {
                     id="search"
                     name="search"
                     label="search"
-                    placeholder='ex: developer, front end'
+                    placeholder='ex:Electrician,Plumber,Carpenter..'
                     value={values.search}
                     onChange={handleChange}
                     error={touched.search && Boolean(errors.search)}
